f693918cac64eebc82903baac890391affc20175
Jakub Holan <38005295+howa003@users.noreply.github.com>
2025-07-22 19:56:13 +0200

    re-connection
    The connect_script.bat now first disconnects any previous connections before trying to connecto to a VPN.

--- src/connect_script.bat
+++ src/connect_script.bat
@@ -1,2 +1,3 @@
 @echo off
+"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" disconnect
 "C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe" -s < private_config.login_info
